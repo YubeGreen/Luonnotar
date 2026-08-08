acbda73916847333f74d18eaf09a72c9de480f88
YubeGreen <ysbssf@gmail.com>
2026-08-08 19:30:46 +1200

    WIP: auto-sync 2026-08-08 19:30:46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.yubegreen.luonnotar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 96
+        versionCode = 97
         versionName = "2.5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "DEVELOPER_NAME", "\"YubeGreen\"")
